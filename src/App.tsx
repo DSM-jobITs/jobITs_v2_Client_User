0367e5d1d344da72c24d310d1a07152cadc4766e
Giljun Bae <bgj0127@gmail.com>
2020-12-17 09:48:29 +0900

    webpack 설정
    no cra

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {hot} from "react-hot-loader";
+
+const App = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default hot(module)(App);
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+        
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+            }
+          }
+        ]
+      }
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
+      },
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  }
+};
