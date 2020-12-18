2602816b904418e4486a5b290b667f8e11541095
Giljun Bae <bgj0127@gmail.com>
2020-12-18 18:19:00 +0900

    [ADD] <GlobalStyle> 스크롤 디자인

--- src/assets/styles/global-styles.ts
+++ src/assets/styles/global-styles.ts
@@ -8,7 +8,22 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     margin-top: 1.875rem; /* 헤더로 겹치는 부분만큼 margin */
-    font-family: 'AppleSDGothic', 'sans-serif';
+    font-family: 'InfinitySans-RegularA1', 'sans-serif';
+    &::-webkit-scrollbar {
+      width: 10px;
+    }
+    &::-webkit-scrollbar-track {
+      border-radius: 5px;
+      background-color: rgba(0,0,0,0);
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 5px;
+      background-color: #006004;
+    }
+    &::-webkit-scrollbar-button {
+      width: 0;
+      height: 0;
+    }
   }
 
   * {
@@ -18,6 +33,18 @@ const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'AppleSDGothic';
     src: url(${require('../fonts/AppleSDGothicNeoB.ttf')});
+  }
+  @font-face {
+    font-family: 'HangeulNuri-Bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/HangeulNuri-Bold.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'InfinitySans-RegularA1';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/InfinitySans-RegularA1.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
   }
 `;
 
