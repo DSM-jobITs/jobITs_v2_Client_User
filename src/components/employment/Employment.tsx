bab495c985aec9edb50a987c9cd76fdec3feaed4
Giljun Bae <bgj0127@gmail.com>
2021-03-04 08:40:29 +0900

    [FIX]<Employment> 코드 분할

--- src/components/employment/Employment.tsx
+++ src/components/employment/Employment.tsx
@@ -1,9 +1,9 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import Header from '../header/Header';
+import FilterBox from './FliterBox';
 import EmploymentItems from './employmentItems/EmploymentItems';
 import * as S from './style';
-import * as C from './comboBoxStyle';
 
 const Employment = () => {
   const [posts, setPosts] = useState([]);
@@ -30,29 +30,7 @@ const Employment = () => {
     <div>
       <Header />
       <S.Main>
-        <S.FilterBox>
-          <S.Text>기업 검색</S.Text>
-          <S.FilterBoxInner>
-            <form>
-              <C.Select id="member" name="member">
-                <option value="">인원</option>
-                <option value="5">5명 이상</option>
-                <option value="20">20명 이상</option>
-                <option value="50">50명 이상</option>
-                <option value="100">100명 이상</option>
-              </C.Select>
-            </form>
-            <div>
-              <S.Search type="text" placeholder="기업 명"></S.Search>
-              <S.Search type="text" placeholder="업무내용" style={{marginLeft:21}}></S.Search>
-              <S.Search type="text" placeholder="지역" style={{marginLeft:21}}></S.Search>
-            </div>
-            <S.BottomWrapper>
-              <S.FilterText>원하시는 검색 조건을 선택(입력)하신 뒤에 결과를 조회해주세요.</S.FilterText>
-              <S.SearchButton>검색하기</S.SearchButton>
-            </S.BottomWrapper>
-          </S.FilterBoxInner>
-        </S.FilterBox>
+        <FilterBox />
         <S.DetailViewWarpper>
           <S.Text>전체 검색 (5)</S.Text>
           <S.InputBox>
@@ -61,7 +39,7 @@ const Employment = () => {
             <S.CheckBox type='radio' name="orderBy" value="deadline"></S.CheckBox>
             <span>마감 순</span>
           </S.InputBox>
-          {
+          {/* {
             loading &&
             <div> loading... </div>
           }
@@ -74,7 +52,8 @@ const Employment = () => {
                 deadline={}
               />
             ))
-          }
+          } */}
+          <EmploymentItems />
         </S.DetailViewWarpper>
       </S.Main>
     </div>
