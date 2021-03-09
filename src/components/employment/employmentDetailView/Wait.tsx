6cd6008c0fe45b0c49a8ac4d8e00a75855c1e9e5
Giljun Bae <bgj0127@gmail.com>
2021-03-09 17:59:28 +0900

    [FEAT]<Employment> Wait

---
+++ src/components/employment/employmentDetailView/Wait.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './waitStyle';
+
+const Wait = () => {
+  return (
+    <S.Container>
+      loading...
+    </S.Container>
+  );
+};
+
+export default Wait;
