43b23f8be3dc3d04fe2fbedb8400873cfb866c5d
Giljun Bae <bgj0127@gmail.com>
2021-03-09 11:13:22 +0900

    [FEAT]<Main/Preview> Link 연결

---
+++ src/components/preview/employment/ItemsWrapper.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as P from '../globalPreviewStlye';
+
+const ItemsWrapper = () => {
+  return (
+    <P.ItemsWrapper>
+      <P.Items>5</P.Items>
+      <P.Items text>마이다스 아이티</P.Items>
+      <P.Items>2020 / 11 / 11</P.Items>
+    </P.ItemsWrapper>
+  );
+};
+
+export default ItemsWrapper;
