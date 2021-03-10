e3c59f03a51951354bd5d3697a9def93d36a488e
Giljun Bae <bgj0127@gmail.com>
2021-03-10 20:05:03 +0900

    [FEAT]<Preview> API set

--- src/components/preview/employment/Employment.tsx
+++ src/components/preview/employment/Employment.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as P from '../globalPreviewStlye';
 import ItemsWrapper from './ItemsWrapper';
+import axios from 'axios';
 
 const Employment = () => {
+  const [datas,setDatas] = useState<any>([]);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const response = await axios.get('url');
+      setDatas(response.data);
+    }
+    fetchData();
+  },[])
+
   return (
     <P.Main>
       <P.Title>채용 의뢰</P.Title>
-      <P.ItemsTitleWrapper name="Employment"> {/* name으로 구별하여 공지사항인지 채용 공고인지 취업 현황인지에 따라 grid를 다르게 준다. */}
+      <P.ItemsTitleWarpper name="Employment"> {/* name으로 구별하여 공지사항인지 채용 공고인지 취업 현황인지에 따라 grid를 다르게 준다. */}
         <P.ItemsTitle name="Number">no</P.ItemsTitle>
         <P.ItemsTitle name="Name">기업</P.ItemsTitle>
         <P.ItemsTitle name="Date">등록일</P.ItemsTitle>
-      </P.ItemsTitleWrapper>
-      <ItemsWrapper />
-      <ItemsWrapper />
-      <ItemsWrapper />
-      <ItemsWrapper />
-      <ItemsWrapper />
+      </P.ItemsTitleWarpper>
+      {
+        datas &&
+        datas.map( (d:any) => (
+          <ItemsWrapper
+            no={d.no}
+            title={d.title}
+            date={d.date}
+          />
+        ))
+      }
     </P.Main>
   );
 };
--- src/components/preview/employment/ItemsWrapper.tsx
+++ src/components/preview/employment/ItemsWrapper.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import * as P from '../globalPreviewStlye';
 
-const ItemsWrapper = () => {
+const ItemsWrapper = ({no,title,date}: any) => {
   return (
-    <P.ItemsWrapper>
+    <P.ItemsWarpper>
       <P.Items>5</P.Items>
       <P.Items text>마이다스 아이티</P.Items>
       <P.Items>2020 / 11 / 11</P.Items>
-    </P.ItemsWrapper>
+    </P.ItemsWarpper>
   );
 };
 
