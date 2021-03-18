1ed45ab8269a82c9ad8ff147c6812dc23699e829
Giljun Bae <bgj0127@gmail.com>
2021-03-18 19:51:04 +0900

    [FIX]<Main> 오타 수정

--- src/components/preview/employment/ItemsWrapper.tsx
+++ src/components/preview/employment/ItemsWrapper.tsx
@@ -3,11 +3,11 @@ import * as P from '../globalPreviewStlye';
 
 const ItemsWrapper = ({no,title,date}: any) => {
   return (
-    <P.ItemsWarpper>
+    <P.ItemsWrapper>
       <P.Items>5</P.Items>
       <P.Items text>마이다스 아이티</P.Items>
       <P.Items>2020 / 11 / 11</P.Items>
-    </P.ItemsWarpper>
+    </P.ItemsWrapper>
   );
 };
 
