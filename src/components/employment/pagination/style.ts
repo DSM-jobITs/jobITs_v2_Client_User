bbbee4b0132c7984739720808052a962bacb7970
Giljun Bae <bgj0127@gmail.com>
2021-04-06 20:02:53 +0900

    [FIX]<Employment> 페이지네이션 style

--- src/components/employment/pagination/style.ts
+++ src/components/employment/pagination/style.ts
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
 
 export const Div = styled.div`
+  position: fixed;
   display: flex;
+  top: 50%;
+  left: 20%;
   justify-content: center;
   align-items: center;
   height: 6.25rem;
 `
 
 export const PageUl = styled.div`
+  display: flex;
+  flex-direction: column;
   text-align:center;
   border-radius:3px;
   color:#000;
@@ -19,11 +24,13 @@ export const PageLi = styled.button`
   display:inline-block;
   font-size:14px;
   padding:5px;
-  border: 1px solid #B7B7B8;
+  border: none;
+  border-radius: 50%;
+  margin-bottom: 10px;
   width:2.5rem;
-  height: 1.875rem;
+  height: 2.5rem;
   cursor:pointer;
-  background: #fff;
+  background: #f2ffe6;
   :focus{
     outline: none;
     color:white;
