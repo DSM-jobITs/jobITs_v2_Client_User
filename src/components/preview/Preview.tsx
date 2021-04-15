46cd9d51182872b8d7774278725515a73e9374f2
Giljun Bae <bgj0127@gmail.com>
2021-04-15 18:49:24 +0900

    [FEAT]<Preview> aleart

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -4,16 +4,18 @@ import * as S from './style';
 import {preview} from '../../assets/images/index';
 
 const Preview = () => {
-
+  const helpMe = () => {
+    alert("준비중인 서비스 입니다.");
+  }
   return (
     <div>
       <S.Preview>
         <S.NoticeImg src={preview} alt="notice 소개 글" />
         <S.PreviewNav>
           <S.Type active>채용 의뢰</S.Type>
-          <S.Type active={false} use={false}>전체 공지</S.Type>
-          <S.Type active={false} use={false}>중요 공지</S.Type>
-          <S.Type active={false} use={false}>취업 현황</S.Type>
+          <S.Type active={false} use={false} onClick={helpMe}>전체 공지</S.Type>
+          <S.Type active={false} use={false} onClick={helpMe}>중요 공지</S.Type>
+          <S.Type active={false} use={false} onClick={helpMe}>취업 현황</S.Type>
         </S.PreviewNav>
         <S.PreviewTemplate>
             <HomeEmployment />
